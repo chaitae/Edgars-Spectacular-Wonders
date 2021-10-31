4144780f69c4f493c4b411689d9172384bcfcbc5
chaitae <chaitaes@gmail.com>
2021-10-31 14:56:39 -0600

    Update beginning and added processing

---
+++ Assets/Scripts/DialogueAnimtorScripts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogueAnimtorScripts : MonoBehaviour
+{
+    Animator animator;
+    [YarnCommand("PlayAnimation")]
+    public void PlayAnimation(string animationName)
+    {
+        Debug.Log("hello");
+        animator.Play(animationName);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
